2d6676ffd772dab4eb9a1ecad4f070d047d721a9
Takahiro Abe <hirotaka081630@gmail.com>
2021-05-20 21:57:45 +0900

    style(frontend): globalなスタイルの設定

--- frontend/react-app/src/App.tsx
+++ frontend/react-app/src/App.tsx
@@ -1,9 +1,10 @@
 import { ChakraProvider, Button } from "@chakra-ui/react";
 import React from "react";
+import theme from "./theme/theme";
 
 export default function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Button colorScheme="teal">ボタン</Button>
     </ChakraProvider>
   );

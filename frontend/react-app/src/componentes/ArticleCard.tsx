315a7b31709d12b41ac42bffb7f00ef5e06a9b26
Takahiro Abe <hirotaka081630@gmail.com>
2021-05-29 14:20:17 +0900

    feat(front): クリックしたらPOSTを実行してDBに保存する機能を実装
    fix #15

--- frontend/react-app/src/componentes/ArticleCard.tsx
+++ frontend/react-app/src/componentes/ArticleCard.tsx
@@ -1,23 +1,40 @@
-import { Box, Container } from "@chakra-ui/layout";
-import { VFC } from "react";
+import { Box, Container, Link } from "@chakra-ui/layout";
+import { memo, useState, VFC } from "react";
 import { ArticleType } from "../types/api/articleType";
-import { Link } from "@chakra-ui/react";
+import { CreateArticleButton } from "./atom/button/CreateArticleButton";
+import axios from "axios";
+import { IconButton } from "@chakra-ui/button";
+import { CheckIcon } from "@chakra-ui/icons";
 
 type Props = {
   article: ArticleType;
 };
 
-export const ArticleCard: VFC<Props> = (props) => {
+export const ArticleCard: VFC<Props> = memo((props) => {
   const { article } = props;
+
+  const createArticle = () => {
+    axios
+      .post<ArticleType>("http://localhost:3000/api/v1/articles", { article })
+      .then((res) => {
+        console.log("success!!");
+      })
+      .catch((err) => {
+        console.log("error");
+      });
+  };
 
   return (
     <Container m="10px">
-      <Box bg="orange.100" borderRadius="20px" p="20px">
+      <Box as="form" bg="orange.100" borderRadius="20px" p="20px">
         <dd>{article.origin_context}</dd>
         <Link color="teal.500" href={article.origin_link} ml="auto" isExternal>
           オリジナルへ移動する
         </Link>
+        <p>{article.twitter_id}</p>
+        {/* ここにボタンを追加する */}
+        <IconButton aria-label="保存ボタン" icon={<CheckIcon />} size="sm" onClick={createArticle} />
       </Box>
     </Container>
   );
-};
+});
--- frontend/react-app/src/componentes/atom/button/CreateArticleButton.tsx
+++ frontend/react-app/src/componentes/atom/button/CreateArticleButton.tsx
@@ -2,10 +2,6 @@ import { IconButton } from "@chakra-ui/button";
 import { CheckIcon } from "@chakra-ui/icons";
 import { memo, VFC } from "react";
 
-export const CreateArticleButton: VFC = memo(() => {
-  const con = () => {
-    console.log("test");
-  };
-
-  return <IconButton aria-label="保存ボタン" icon={<CheckIcon />} size="sm" type="submit" onClick={con} />;
+export const CreateArticleButton: VFC = memo((props) => {
+  return <IconButton aria-label="保存ボタン" icon={<CheckIcon />} size="sm" />;
 });

7bde01b3b9dc628e64a11f96c907067f71903b73
Takahiro Abe <hirotaka081630@gmail.com>
2021-06-01 08:06:34 +0900

    feat(backend): articlesのsearchアクションに対するroutingを設定

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :articles, only: [:index, :create, :destroy]
+      resources :articles do
+        collection do
+          get 'search'
+        end
+      end
       resources :favorites, only: [:index]
       resources :timelines, only: [:index]
     end

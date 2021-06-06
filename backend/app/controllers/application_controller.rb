d478303fe047eb9aa433978e249d0c6249f6bc5b
Takahiro Abe <hirotaka081630@gmail.com>
2021-06-06 17:54:40 +0900

    fix(backend): twitter apiのエラー回避のために一旦コメントアウト

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,13 +1,12 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
-  protect_from_forgery
-  before_action :skip_session
-  skip_before_action :verify_authenticity_token, if: :devise_controller?
+  # protect_from_forgery before_action :skip_session
+  # skip_before_action :verify_authenticity_token, if: :devise_controller?
 
-  protected
-    def skip_session
-      request.session_options[:skip] = true
-    end
+  # protected
+  #   def skip_session
+  #     request.session_options[:skip] = true
+  #   end
 
   private
 

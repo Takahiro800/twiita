a64d68568d3d0b0f515c70ea1c45239e615376d1
Takahiro Abe <hirotaka081630@gmail.com>
2021-06-09 16:24:04 +0900

    fix(backend): 開発用のDBもpgに変更

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2021_06_06_011101) do
 
-  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "twitter_id"
     t.string "origin_user"
@@ -23,7 +26,7 @@ ActiveRecord::Schema.define(version: 2021_06_06_011101) do
     t.string "icon_url"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
     t.string "encrypted_password", default: "", null: false

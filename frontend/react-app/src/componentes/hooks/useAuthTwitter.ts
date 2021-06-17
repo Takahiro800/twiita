c34df4b6ac3725e90b69e4504c818c54a31cec32
Takahiro Abe <hirotaka081630@gmail.com>
2021-06-17 21:32:12 +0900

    fix(front): twitter認証画面を同じページで表示するように修正

--- frontend/react-app/src/componentes/hooks/useAuthTwitter.ts
+++ frontend/react-app/src/componentes/hooks/useAuthTwitter.ts
@@ -2,11 +2,12 @@ import { Axios } from "../../Axios";
 
 export const useAuthTwitter = () => {
   const authTwitter = () => {
-    Axios.get("/omniauth/twitter", {
+    Axios.get("/api/v1/get_oauth_twitter_url", {
       withCredentials: true,
     })
       .then((res) => {
         console.log(res);
+        window.location.href = res.data.oauth_url;
       })
       .catch((err) => {
         console.log("error!!!!!!!!!!!!");

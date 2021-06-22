da0ec6317205e5d37bb21a78b0171807c5fdc355
Takahiro Abe <hirotaka081630@gmail.com>
2021-06-22 20:46:20 +0900

    fix(backend/useauthtwitter): twitter認証後に認証画面を閉じたら、home画面に戻るように修正

--- frontend/react-app/src/componentes/hooks/useAuthTwitter.ts
+++ frontend/react-app/src/componentes/hooks/useAuthTwitter.ts
@@ -1,17 +1,35 @@
+import { useHistory } from "react-router-dom";
 import { Axios } from "../../Axios";
 
 export const useAuthTwitter = () => {
+  const history = useHistory();
+
+  const CheckLoginStatusTwitter = (window: Window | null) => {
+    if (window!.closed) {
+      alert("test");
+    } else {
+      setTimeout(function () {
+        CheckLoginStatusTwitter(window);
+      }, 1000);
+    }
+  };
+
   const authTwitter = () => {
     Axios.get("/api/v1/get_oauth_twitter_url", {
       withCredentials: true,
     })
       .then((res) => {
-        console.log(res);
-        window.location.href = res.data.oauth_url;
+        const windowLogin = window.open(res.data.oauth_url);
+        CheckLoginStatusTwitter(windowLogin);
+
+        window.focus();
       })
       .catch((err) => {
         console.log("error!!!!!!!!!!!!");
         console.log(err);
+      })
+      .finally(() => {
+        history.push("/home");
       });
   };
 
